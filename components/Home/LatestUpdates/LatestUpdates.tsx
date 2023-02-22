e82bcbb6c81fe707f2ec7b90d6ccf2ff52672724
Adam Ali <adam_ali_96@hotmail.co.uk>
2023-02-22 18:04:56 +0000

    add facebook plugin

--- components/Home/LatestUpdates/LatestUpdates.tsx
+++ components/Home/LatestUpdates/LatestUpdates.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Card, CardActions, CardContent, Divider, Typography } from '@mui/material'
 
 type Props = {}
+// <div class="fb-page" data-href="https://www.facebook.com/MasjideBilalPrestwich" data-show-posts="true" data-width="500" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+// <blockquote cite="https://www.facebook.com/MasjideBilalPrestwich" class="fb-xfbml-parse-ignore">
+//   <a href="https://www.facebook.com/MasjideBilalPrestwich">Facebook</a>
+// </blockquote>
+// </div>
 
+const mobileScript = `
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0" nonce="random-nonce"></script>
+<div class="fb-page" data-href="https://www.facebook.com/MasjideBilalPrestwich" data-show-posts="true" data-width="340" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/MasjideBilalPrestwich" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/MasjideBilalPrestwich">Masjid E Bilal Prestwich</a></blockquote></div>
+`
+const desktopScript = `
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0" nonce="random-nonce"></script>
+<div class="fb-page" data-href="https://www.facebook.com/MasjideBilalPrestwich" data-show-posts="true" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/MasjideBilalPrestwich" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/MasjideBilalPrestwich">Masjid E Bilal Prestwich</a></blockquote></div>
+`
 const LatestUpdatesCard = () => {
+  const [isMobile, setisMobile] = useState(false)
+  const [isMounted, setIsMounted] = useState(false) // Need this for the react-tooltip
+
+  useEffect(() => {
+    setisMobile(/Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent))
+    setIsMounted(true)
+  }, [])
+
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
+        <div className="flex justify-center">
+          {isMounted && <div dangerouslySetInnerHTML={{ __html: isMobile ? mobileScript : desktopScript }} />}
+        </div>
         <Typography variant="h2">Tafseer Classes</Typography>
         <Typography variant="body1">
           Our Tafseer classes take place every Friday after Isha, where we go through the meaning of the 30th Juz.
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,15 @@ function _head({}: Props) {
       <body>
         <Main />
         <NextScript />
+
+        <div id="fb-root"></div>
+        <script
+          async
+          defer
+          crossOrigin="anonymous"
+          src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0"
+          nonce="random-nonce"
+        ></script>
       </body>
     </Html>
   )
