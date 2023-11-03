901eb47f7d7b2dc7bf528c0da043c0820fa61c9f
Adam Ali <adam@pfida.com>
2023-11-03 23:51:21 +0000

    update img

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,15 +3,21 @@ import { Box } from '@mui/system'
 // import './Hero.css'
 import MasjideBilal from '../../../public/MasjideBilal.png'
 import Image from 'next/image'
+import { ThemeProvider, Typography, useTheme } from '@mui/material'
 
 type Props = {}
 
 const Hero = (props: Props) => {
+  const theme = useTheme()
+  const Hadith = `Abu Huraira reported that the Prophet (ﷺ), said, “Whoever goes to the mosque in the morning and evening, Allah will prepare for him a place in Paradise for every morning and evening.” [Bukhari and Muslim] `
   return (
     <>
-      <Box sx={{ bgcolor: '#cfe8fc', height: '300px', overflow: 'hidden' }}>
+      <Box sx={{ bgcolor: '#1b4805', height: '300px', overflow: 'hidden' }}>
         <div className="relative w-full	h-full">
-          <Image src={MasjideBilal} layout="responsive" alt="Masjid Bilal" />
+          <img src="/mBilal.png" className="M-bilal-img" />
+          <Typography variant="body2" className="bottom-2.5 absolute px-2 py-1" style={{ backgroundColor: '#cef2d8' }}>
+            {Hadith}
+          </Typography>
         </div>
       </Box>
     </>
--- theme/theme.ts
+++ theme/theme.ts
@@ -20,6 +20,7 @@ export const theme = createTheme({
       // dark: will be calculated from palette.secondary.main,
       // contrastText: '#ffcc00'
     },
+    // MasjidBialalGreen: '#cef2d8',
     // Used by `getContrastText()` to maximize the contrast between
     // the background and the text.
     contrastThreshold: 3,
